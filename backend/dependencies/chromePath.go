ca8d4f3ac7e6f451d4ed2d635dd204343deaf844
washedDeveloper <patrick.brophy2@gmail.com>
2021-03-26 13:20:52 -0400

    added path resolution for mac, windows, and linux (default)

--- backend/dependencies/chromePath.go
+++ backend/dependencies/chromePath.go
@@ -1,19 +1,49 @@
 package dependencies
 
 import (
+	"os"
 	"runtime"
 )
 
 func GetPath() string {
-	os := runtime.GOOS
-	switch os {
+	var paths []string
+	system := runtime.GOOS
+	switch system {
 	case "windows":
-		return "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+		paths = []string{
+			os.Getenv("LocalAppData") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("ProgramFiles") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Google/Chrome/Application/chrome.exe",
+			os.Getenv("LocalAppData") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Chromium/Application/chrome.exe",
+			os.Getenv("ProgramFiles(x86)") + "/Microsoft/Edge/Application/msedge.exe",
+		}
 	case "darwin":
-		return ""
-	case "linux":
-		return ""
+		paths = []string{
+			"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+			"/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
+			"/Applications/Chromium.app/Contents/MacOS/Chromium",
+			"/usr/bin/google-chrome-stable",
+			"/usr/bin/google-chrome",
+			"/usr/bin/chromium",
+			"/usr/bin/chromium-browser",
+		}
 	default:
-		return ""
+		paths = []string{
+			"/usr/bin/google-chrome-stable",
+			"/usr/bin/google-chrome",
+			"/usr/bin/chromium",
+			"/usr/bin/chromium-browser",
+			"/snap/bin/chromium",
+		}
 	}
+
+	for _, path := range paths {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			continue
+		}
+		return path
+	}
+	return ""
 }
